9f86e3ed145dbfb273617427604daf03c1370615
Mohammad Pourhosein <m.pourhosein7777@gmail.com>
2026-06-16 13:49:20 +0330

    fix main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import MainPage from "@/Components/mainPage";
 
 export default function Home() {
 
-  return (
-    <main className="w-full mx-auto flex items-center justify-center overflow-hidden">
-      <MainPage />
-    </main>
-  );
+  return <MainPage />;
 }
